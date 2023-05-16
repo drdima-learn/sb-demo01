61128e0b1df5eb8c631229c6e4e240dc3aec75b2
drdima <drdima2@gmail.com>
2023-05-16 15:54:16 -0500

    add jacoco plugin to maven added some tests to be more coverage

--- src/main/java/com/rubincomputers/sb_demo01/config/SpringProfiles.java
+++ src/main/java/com/rubincomputers/sb_demo01/config/SpringProfiles.java
@@ -1,6 +1,10 @@
 package com.rubincomputers.sb_demo01.config;
 
 public class SpringProfiles {
+
+    private SpringProfiles() {
+    }
+
     public static final String
             DEV = "dev";
 
--- src/main/java/com/rubincomputers/sb_demo01/web/util/Util.java
+++ src/main/java/com/rubincomputers/sb_demo01/web/util/Util.java
@@ -3,6 +3,10 @@ package com.rubincomputers.sb_demo01.web.util;
 import javax.servlet.http.HttpServletRequest;
 
 public class Util {
+
+    private Util() {
+    }
+
     public static String getFullUrl(HttpServletRequest req){
         String fullUrl = req.getRequestURL().toString();
 
